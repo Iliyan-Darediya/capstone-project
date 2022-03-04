bfc882c8a101d796dc510974f8fe71f7b0626e50
Iliyan-Darediya <63348176+Iliyan-Darediya@users.noreply.github.com>
2022-03-04 15:15:15 -0800

    create newSession component

---
+++ src/components/NewSession.js
@@ -0,0 +1,11 @@
+function NewSession() {
+    return (
+        <div className="bg-blue-300">
+        <header className="App-header">
+            <h3>Hello from NewSession</h3>
+        </header>
+        </div>
+    );
+}
+  
+export default NewSession;
