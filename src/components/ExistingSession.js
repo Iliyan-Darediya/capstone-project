44e1aa2cdd9435ceb92ba4cac22e3aed030814b8
Iliyan-Darediya <63348176+Iliyan-Darediya@users.noreply.github.com>
2022-03-04 15:38:27 -0800

    implement context and children in Existing Session

--- src/components/ExistingSession.js
+++ src/components/ExistingSession.js
@@ -1,8 +1,43 @@
+import { Link } from "react-router-dom";
+import {useContext} from "react";
+import {Context} from '../Context'
+
 function ExistingSession() {
+    const {currentId} = useContext(Context)
+    const {name} = useContext(Context)
+    const {setCurrentId} = useContext(Context)
+    const {setName} = useContext(Context)
+
+    const handleId=event=>{
+        setCurrentId((event.target.value).toString())
+    }
+    const handleName=event=>{
+        setName((event.target.value).toString())
+    }
+
     return (
-        <div className="bg-blue-300">
+        <div className="App">
         <header className="App-header">
-            <h3>Hello from ExistingSession</h3>
+            <h3>Hello from ExistingSession the session Id is {currentId}</h3>
+            <input 
+                type="text" 
+                value={currentId} 
+                name="id" 
+                placeholder="EnterId" 
+                onChange={handleId} 
+            />
+            <input 
+                type="text" 
+                value={name} 
+                name="name" 
+                placeholder="Enter Name" 
+                onChange={handleName} 
+            />
+            <button
+                className="flex items-center px-6 py-2  transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none"
+            >
+                <Link to = "/chooser">Go to Chooser</Link>
+            </button>
         </header>
         </div>
     );
