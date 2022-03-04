03fc420abb5d7fe6e3071310169195c2dcac2aa5
Iliyan-Darediya <63348176+Iliyan-Darediya@users.noreply.github.com>
2022-03-04 15:08:42 -0800

    Separate the App to be in its own file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-const App = (
-    <h1>
-        This is the basic index page from React
-    </h1>
-)
+import './index.css';
+import App from './App';
 
-ReactDOM.render(App, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"))
