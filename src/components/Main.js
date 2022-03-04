3aac356c952952a0942d75272fce5bd071afbee5
Iliyan-Darediya <63348176+Iliyan-Darediya@users.noreply.github.com>
2022-03-04 15:13:22 -0800

    Create main component

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+function Main() {
+    return (
+        <div className="bg-blue-300">
+        <header className="App-header">
+            <h3>Hello from Main</h3>
+        </header>
+        </div>
+    );
+}
+  
+export default Main;
