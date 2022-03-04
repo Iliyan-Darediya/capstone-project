dad58b70d485825dae44b51042b40d443c2d3d4f
Iliyan-Darediya <63348176+Iliyan-Darediya@users.noreply.github.com>
2022-03-04 15:16:35 -0800

    create existing session component

---
+++ src/components/ExistingSession.js
@@ -0,0 +1,11 @@
+function ExistingSession() {
+    return (
+        <div className="bg-blue-300">
+        <header className="App-header">
+            <h3>Hello from ExistingSession</h3>
+        </header>
+        </div>
+    );
+}
+  
+export default ExistingSession;
